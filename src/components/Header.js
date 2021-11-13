1120dd29fd84ffc944d14dd0b1b936bd52cfc963
ZooWee <condreaychris@gmail.com>
2021-11-12 20:01:43 -0800

    files created

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  const header = 'Chris Condreay';
+
+  return (
+    <div className="header">
+      <h2>{header}</h2>
+    </div>
+  );  
+}
+
+export default Header;
